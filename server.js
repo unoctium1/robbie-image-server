4d76dba36c83191fae361d4b656448697d4a177c
Abel Waller <awaller8@bcit.ca>
2020-10-26 09:51:58 -0700

    Persist db

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ global.__basedir = __dirname;
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-//db.sequelize.sync();
-
+db.sequelize.sync();
+/*
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
-
+*/
 let port = 3000;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`)
